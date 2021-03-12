5a5af11fef800b77637cd5ac8b8f34afe3b7e13a
Richard Walsh <richwalsh2@googlemail.com>
2021-03-12 08:26:47 +0000

    initial commit

---
+++ e_m_adventures/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Home from './containers/Home/Home';
+
+require('./App.css');
+require('./Variables.css');
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
