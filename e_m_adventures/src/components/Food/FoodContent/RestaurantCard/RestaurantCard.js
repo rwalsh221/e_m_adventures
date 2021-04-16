5a6e3bb57626bee3594ab1d18478b3016c424bf8
Richard Walsh <richwalsh2@googlemail.com>
2021-04-16 18:41:21 +0100

    commit

--- e_m_adventures/src/components/Food/FoodContent/RestaurantCard/RestaurantCard.js
+++ e_m_adventures/src/components/Food/FoodContent/RestaurantCard/RestaurantCard.js
@@ -3,6 +3,7 @@ import React from 'react';
 import classes from './RestaurantCard.module.css';
 
 const RestaurantCard = (props) => {
+  console.log(classes);
   return (
     <div className={classes.sidebarCard}>
       <img
@@ -18,15 +19,3 @@ const RestaurantCard = (props) => {
 };
 
 export default RestaurantCard;
-
-/* <div className={classes.sidebarCard}>
-            <img
-              className={classes.sidebarCardImg}
-              src={babar}
-              alt={'babar'}
-            ></img>
-            <h6>Babar Elephant</h6>
-            <a href={'http://www.babarelephant.co.uk'}>babarelephant.co.uk/</a>
-            <p>+441524388670</p>
-          </div>
-        </div> */
