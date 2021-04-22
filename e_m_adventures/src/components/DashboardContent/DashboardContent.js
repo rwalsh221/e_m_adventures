8d739aae13548da7135d31282fe5d362c93e757f
Richard Walsh <richwalsh2@googlemail.com>
2021-04-22 20:09:02 +0100

    format booking in database. add future and past booking section. arrange by date

--- e_m_adventures/src/components/DashboardContent/DashboardContent.js
+++ e_m_adventures/src/components/DashboardContent/DashboardContent.js
@@ -9,7 +9,8 @@ import classes from './DashboardContent.module.css';
 
 const DashboardContent = () => {
   // const [error, setError] = useState('');
-  const [bookings, setBookings] = useState(null);
+  const [bookingsFuture, setBookingsFuture] = useState(null);
+  const [bookingsPast, setBookingsPast] = useState(null);
 
   const database =
     'https://e-m-adventures-development-default-rtdb.europe-west1.firebasedatabase.app/';
@@ -17,6 +18,10 @@ const DashboardContent = () => {
   const { currentUser, logout } = useAuth();
 
   const history = useHistory();
+
+  const sortArray = (arr, key = 'checkIn') => {
+    arr.sort((a, b) => parseFloat(a[key]) - parseFloat(b[key]));
+  };
 
   const handleLogout = async () => {
     // setError('');
@@ -32,15 +37,43 @@ const DashboardContent = () => {
   useEffect(() => {
     const fetchBookings = async () => {
       try {
+        // GET BOOKINGS FROM DATABASE
         const myBookings = await fetch(
           `${database}/users/${currentUser.uid}/booking.json?auth=${process.env.REACT_APP_FIREBASE_DATABASE_SECRET}`
         );
 
-        console.log(myBookings);
         let myBookingsData = await myBookings.json();
-        console.log(myBookingsData);
-        setBookings(myBookingsData);
-        // console.log(bookings);
+        let myBookingsDataArrFuture = [];
+        let myBookingsDataArrPast = [];
+
+        // PUSH BOOKINGS TOO ARRAY
+        for (let key in myBookingsData) {
+          console.log(myBookingsData);
+          console.log(myBookingsData[key].checkIn);
+          const date = new Date();
+          console.log(date.getTime());
+          if (myBookingsData[key].checkIn > date.getTime()) {
+            console.log(myBookingsData[key].checkIn);
+
+            console.log('if');
+            myBookingsDataArrFuture.push(myBookingsData[key]);
+          } else if (myBookingsData[key].checkIn < date.getTime()) {
+            console.log(myBookingsData[key].checkIn);
+            console.log('else');
+            myBookingsDataArrPast.push(myBookingsData[key]);
+          }
+        }
+
+        // SORT ARRAY BY DATE
+        sortArray(myBookingsDataArrFuture);
+        sortArray(myBookingsDataArrPast);
+        // myBookingsDataArrFuture.sort(
+        //   (a, b) => parseFloat(a.checkIn) - parseFloat(b.checkIn)
+        // );
+
+        // SET STATES WITH SORTED ARRAY
+        setBookingsFuture(myBookingsDataArrFuture);
+        setBookingsPast(myBookingsDataArrPast);
       } catch (error) {
         console.error(error);
       }
@@ -48,17 +81,44 @@ const DashboardContent = () => {
     fetchBookings();
   }, [currentUser.uid]);
 
-  let content = [];
+  let contentFuture = [];
+  let contentPast = [];
 
-  for (let key in bookings) {
-    content.push(
+  for (let key in bookingsFuture) {
+    contentFuture.push(
       <div className={classes.bookingCardContent} key={key}>
         <ul>
           <li>
-            Ref:<span className={classes.ref}>{bookings[key].bookingRef}</span>
+            Ref:
+            <span className={classes.ref}>
+              {bookingsFuture[key].bookingRef}
+            </span>
           </li>
-          <li>{`Check In: ${formatDate(bookings[key].checkIn / 1000)}`}</li>
-          <li>{`Check Out: ${formatDate(bookings[key].checkOut / 1000)}`}</li>
+          <li>{`Check In: ${formatDate(
+            bookingsFuture[key].checkIn / 1000
+          )}`}</li>
+          <li>{`Check Out: ${formatDate(
+            bookingsFuture[key].checkOut / 1000
+          )}`}</li>
+        </ul>
+      </div>
+    );
+  }
+
+  for (let key in bookingsPast) {
+    contentPast.push(
+      <div className={classes.bookingCardContent} key={key}>
+        <ul>
+          <li>
+            Ref:
+            <span className={classes.ref}>
+              {bookingsFuture[key].bookingRef}
+            </span>
+          </li>
+          <li>{`Check In: ${formatDate(bookingsPast[key].checkIn / 1000)}`}</li>
+          <li>{`Check Out: ${formatDate(
+            bookingsPast[key].checkOut / 1000
+          )}`}</li>
         </ul>
       </div>
     );
@@ -66,7 +126,7 @@ const DashboardContent = () => {
 
   console.log(currentUser.uid);
   return (
-    <main className={'contentGrid'}>
+    <main className={classes.grid}>
       <h1 className={classes.header}>{currentUser.email}'s DashBoard</h1>
       <div className={classes.profile}>
         <h2 className={classes.profileHeading}>Profile</h2>
@@ -85,9 +145,25 @@ const DashboardContent = () => {
           Update Profile
         </Link>
       </div>
-      <h3 className={classes.bookingHeading}>Your Bookings</h3>
-      <div className={classes.bookingCard}>
-        {content.length === 0 ? null : content}
+      <h3 className={classes.bookingHeading}>Your Upcoming Bookings</h3>
+      <div className={classes.futureDateContainer}>
+        {contentFuture.length === 0 ? (
+          <div className={classes.bookingCardContent}>
+            YOU HAVE NO UPCOMING BOOKINGS
+          </div>
+        ) : (
+          contentFuture
+        )}
+      </div>
+      <h3 className={classes.bookingHeading}>Your past Bookings</h3>
+      <div className={classes.pastDateContainer}>
+        {contentPast.length === 0 ? (
+          <div className={classes.bookingCardContent}>
+            YOU HAVE NO PAST BOOKINGS
+          </div>
+        ) : (
+          contentPast
+        )}
       </div>
     </main>
   );
