e795da80d6cd1480033bcfc166cc9f073b83a4f1
Richard Walsh <richwalsh2@googlemail.com>
2021-11-21 18:45:14 +0000

    lint index.js

--- e_m_adventures/src/index.js
+++ e_m_adventures/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './App';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import rootReducer from './store/index';
+
+import App from './App';
 
 const store = configureStore({
   reducer: rootReducer,
