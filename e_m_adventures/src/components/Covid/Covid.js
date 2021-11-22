0a573d7ae9f93ad8b6e1c41468be4365e5a13238
Richard Walsh <richwalsh2@googlemail.com>
2021-11-22 18:19:21 +0000

    lint covid folder

--- e_m_adventures/src/components/Covid/Covid.js
+++ e_m_adventures/src/components/Covid/Covid.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 import classes from './Covid.module.css';
 
-const Covid = (props) => {
-  return (
-    <div className={classes.covid}>
-      <a href="/covid">
-        <p className={classes.content}>View Our Covid Response</p>
-      </a>
-    </div>
-  );
-};
+const Covid = () => (
+  <div className={classes.covid}>
+    <a href="/covid">
+      <p className={classes.content}>View Our Covid Response</p>
+    </a>
+  </div>
+);
 
 export default Covid;
--- e_m_adventures/src/components/Covid/CovidContent/CovidContent.js
+++ e_m_adventures/src/components/Covid/CovidContent/CovidContent.js
@@ -9,47 +9,41 @@ import travel from '../../../assets/img/covid/travel.jpg';
 
 import classes from './CovidContent.module.css';
 
-const CovidContent = (props) => {
-  return (
-    <section className={classes.covidContent}>
-      <div className={classes.headerContainer}>
-        <img src={covidHero} alt={'covid'}></img>
-        <h2 className={classes.header}>Our Covid Response</h2>
-      </div>
-      <div className={classes.covidInfo}>
-        <LoremIpsum p={2} />
-      </div>
-      <div className={classes.covidResponse}>
-        <img src={clean} alt={'cleaning'}></img>
-        <a href={'/covid'}>
-          <h5>Our Cleaning Policy</h5>
-        </a>
-      </div>
-      <div className={classes.covidResponse}>
-        <img src={distance} alt={'distance'}></img>
+const CovidContent = () => (
+  <section className={classes.covidContent}>
+    <div className={classes.headerContainer}>
+      <img src={covidHero} alt="covid" />
+      <h2 className={classes.header}>Our Covid Response</h2>
+    </div>
+    <div className={classes.covidInfo}>
+      <LoremIpsum p={2} />
+    </div>
+    <div className={classes.covidResponse}>
+      <img src={clean} alt="cleaning" />
+      <a href="/covid">
+        <h5>Our Cleaning Policy</h5>
+      </a>
+    </div>
+    <div className={classes.covidResponse}>
+      <img src={distance} alt="distance" />
 
-        <a href={'/covid'}>
-          <h5>Our Social Distancing Policy</h5>
-        </a>
-      </div>
-      <div className={classes.covidResponse}>
-        <img src={travel} alt={'travel'}></img>
-        <a
-          href={
-            'https://www.lancashire.gov.uk/health-and-social-care/your-health-and-wellbeing/coronavirus/'
-          }
-        >
-          <h5>Local Travel Restrictions</h5>
-        </a>
-      </div>
-      <div className={classes.covidResponse}>
-        <img src={stayhome} alt={'stay home'}></img>
-        <a href={'https://www.gov.uk/coronavirus'}>
-          <h5>Current Covid-19 Information</h5>
-        </a>
-      </div>
-    </section>
-  );
-};
+      <a href="/covid">
+        <h5>Our Social Distancing Policy</h5>
+      </a>
+    </div>
+    <div className={classes.covidResponse}>
+      <img src={travel} alt="travel" />
+      <a href="https://www.lancashire.gov.uk/health-and-social-care/your-health-and-wellbeing/coronavirus/">
+        <h5>Local Travel Restrictions</h5>
+      </a>
+    </div>
+    <div className={classes.covidResponse}>
+      <img src={stayhome} alt="stay home" />
+      <a href="https://www.gov.uk/coronavirus">
+        <h5>Current Covid-19 Information</h5>
+      </a>
+    </div>
+  </section>
+);
 
 export default CovidContent;
