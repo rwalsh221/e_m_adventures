118c3d1a7a400174c8b025dde8b1b2bbf8d5c963
Richard Walsh <richwalsh2@googlemail.com>
2021-11-22 17:50:05 +0000

    lint Footer folder

--- e_m_adventures/src/components/Footer/AccommodationFooter.js/AccommodationFooter.js
+++ e_m_adventures/src/components/Footer/AccommodationFooter.js/AccommodationFooter.js
@@ -2,28 +2,27 @@ import React from 'react';
 
 import classes from './AccommodationFooter.module.css';
 
-const AccommodationFooter = () => {
-  return (
-    <section className={classes.accommodationFooter}>
-      <div className={classes.accommodationFooterTop} />
-      <h2>Explore our other Locations</h2>
-      <div className={classes.footerLocationContainer}>
-        <ul>
-          <li>Location</li>
-          <li>Location</li>
-          <li>Location</li>
-          <li>Location</li>
-          <li>Location</li>
-          <li>Location</li>
-          <li>Location</li>
-          <li>Location</li>
-          <li>Location</li>
-          <li>Location</li>
-          <li>Location</li>
-          <li>Location</li>
-        </ul>
-      </div>
-    </section>
-  );
-};
+const AccommodationFooter = () => (
+  <section className={classes.accommodationFooter}>
+    <div className={classes.accommodationFooterTop} />
+    <h2>Explore our other Locations</h2>
+    <div className={classes.footerLocationContainer}>
+      <ul>
+        <li>Location</li>
+        <li>Location</li>
+        <li>Location</li>
+        <li>Location</li>
+        <li>Location</li>
+        <li>Location</li>
+        <li>Location</li>
+        <li>Location</li>
+        <li>Location</li>
+        <li>Location</li>
+        <li>Location</li>
+        <li>Location</li>
+      </ul>
+    </div>
+  </section>
+);
+
 export default AccommodationFooter;
--- e_m_adventures/src/components/Footer/Footer.js
+++ e_m_adventures/src/components/Footer/Footer.js
@@ -1,61 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import classes from './Footer.module.css';
 
-const Footer = (props) => {
-  return (
-    <footer className={classes.footer}>
-      {props.children}
-      <div className={classes.footerTop}>
-        <div className={classes.footerTopContent}>
-          <Link to={'/ourplace'}>Our Place</Link>
-          <Link to={'/'}>Home</Link>
-        </div>
-        <div className={classes.footerTopContent}>
-          <Link to={'/explore'}>Explore</Link>
-          <Link to={'/'}>Investors</Link>
-        </div>
-        <div className={classes.footerTopContent}>
-          <Link to={'/food'}>Local Eats</Link>
-          <Link to={'/'}>Careers</Link>
-        </div>
-        <div className={classes.footerTopContent}>
-          <Link to={'/login'}>Login</Link>
-          <Link to={'/covid'}>Covid 19 </Link>
-        </div>
+const Footer = ({ children }) => (
+  <footer className={classes.footer}>
+    {children}
+    <div className={classes.footerTop}>
+      <div className={classes.footerTopContent}>
+        <Link to="/ourplace">Our Place</Link>
+        <Link to="/">Home</Link>
       </div>
-      <div className={classes.footerBtm}>
-        <div className={classes.footerBtmContent}>
-          <p>&copy; 2021 e &amp; m Adventures. All rights reserved</p>
-          <p>
-            Website built and designed by{' '}
-            <a href={'https://github.com/rwalsh221'}>Consultio/Consultius</a>
-          </p>
-        </div>
-        <div className={classes.footerBtmContentIcon}>
-          <a href={'https://github.com/rwalsh221'}>
-            <ion-icon name="logo-github"></ion-icon>
-          </a>
-        </div>
-        <div className={classes.footerBtmContentIcon}>
-          <a href={'https://en-gb.facebook.com/'}>
-            <ion-icon name="logo-facebook"></ion-icon>
-          </a>
-        </div>
-        <div className={classes.footerBtmContentIcon}>
-          <a href={'https://twitter.com/'}>
-            <ion-icon name="logo-twitter"></ion-icon>
-          </a>
-        </div>
-        <div className={classes.footerBtmContentIcon}>
-          <a href={'https://www.instagram.com/'}>
-            <ion-icon name="logo-instagram"></ion-icon>
-          </a>
-        </div>
+      <div className={classes.footerTopContent}>
+        <Link to="/explore">Explore</Link>
+        <Link to="/">Investors</Link>
       </div>
-    </footer>
-  );
+      <div className={classes.footerTopContent}>
+        <Link to="/food">Local Eats</Link>
+        <Link to="/">Careers</Link>
+      </div>
+      <div className={classes.footerTopContent}>
+        <Link to="/login">Login</Link>
+        <Link to="/covid">Covid 19 </Link>
+      </div>
+    </div>
+    <div className={classes.footerBtm}>
+      <div className={classes.footerBtmContent}>
+        <p>&copy; 2021 e &amp; m Adventures. All rights reserved</p>
+        <p>
+          Website built and designed by{' '}
+          <a href="https://github.com/rwalsh221">Consultio/Consultius</a>
+        </p>
+      </div>
+      <div className={classes.footerBtmContentIcon}>
+        <a href="https://github.com/rwalsh221">
+          <ion-icon name="logo-github" />
+        </a>
+      </div>
+      <div className={classes.footerBtmContentIcon}>
+        <a href="https://en-gb.facebook.com/">
+          <ion-icon name="logo-facebook" />
+        </a>
+      </div>
+      <div className={classes.footerBtmContentIcon}>
+        <a href="https://twitter.com/">
+          <ion-icon name="logo-twitter" />
+        </a>
+      </div>
+      <div className={classes.footerBtmContentIcon}>
+        <a href="https://www.instagram.com/">
+          <ion-icon name="logo-instagram" />
+        </a>
+      </div>
+    </div>
+  </footer>
+);
+
+Footer.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default Footer;
