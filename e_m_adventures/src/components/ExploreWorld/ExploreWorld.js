769de510fe72593ca9fc7bd0306265293c1222ad
Richard Walsh <richwalsh2@googlemail.com>
2021-11-22 18:13:23 +0000

    lint exploreWorld folder

--- e_m_adventures/src/components/ExploreWorld/ExploreWorld.js
+++ e_m_adventures/src/components/ExploreWorld/ExploreWorld.js
@@ -9,35 +9,23 @@ import walesFlag from '../../assets/img/walesFlag.svg';
 import nIrelandHero from '../../assets/img/nIrelandHero.jpg';
 import nIrelandFlag from '../../assets/img/nIrelandFlag.svg';
 
-const ExploreWorld = (props) => {
+const ExploreWorld = () => {
   const locations = [
     { img: scotlandHero, flag: scotlandFlag, key: '1' },
     { img: walesHero, flag: walesFlag, key: '2' },
     { img: nIrelandHero, flag: nIrelandFlag, key: '3' },
   ];
 
-  const content = locations.map((element) => {
-    return (
-      // <Link to={'/world'}>
-      <Link
-        to={'/world'}
-        className={classes.exploreWorldCard}
-        key={element.key}
-      >
-        <img
-          className={classes.exploreWorldHero}
-          alt={'explore the world'}
-          src={element.img}
-        ></img>
-        <img
-          className={classes.exploreWorldFlag}
-          alt={'flag'}
-          src={element.flag}
-        ></img>
-      </Link>
-      // </Link>
-    );
-  });
+  const content = locations.map((element) => (
+    <Link to="/world" className={classes.exploreWorldCard} key={element.key}>
+      <img
+        className={classes.exploreWorldHero}
+        alt="explore the world"
+        src={element.img}
+      />
+      <img className={classes.exploreWorldFlag} alt="flag" src={element.flag} />
+    </Link>
+  ));
 
   return (
     <section className={classes.exploreWorld}>
--- e_m_adventures/src/components/ExploreWorld/ExploreWorldContent/ExploreWorldContent.js
+++ e_m_adventures/src/components/ExploreWorld/ExploreWorldContent/ExploreWorldContent.js
@@ -10,106 +10,71 @@ import scotFlag from '../../../assets/img/scotlandFlag.png';
 import mustSee from '../../../assets/img/exploreWorld/mustSee.jpg';
 import classes from './ExploreWorldContent.module.css';
 
-const ExploreWorldContent = (props) => {
-  return (
-    <main className={'contentGrid'}>
-      <div className={classes.heading}>
-        <h1 className={classes.headingH1}>Explore Scotland</h1>
-        <LoremIpsum p={1} />
-      </div>
-      {/* <div className={classes.paraSubL}>
-        <h4 className={classes.paraHeading}>Explore Scotish Nature</h4>
-        <LoremIpsum p={1} />
-        <img
-          className={`${classes.subImgL} ${classes.subImg}`}
-          src={scotNature}
-          alt={'sub'}
-        ></img>
-        <LoremIpsum p={2} />
-      </div> */}
-      <ContentPara
-        position={'left'}
-        imgPosition={'imgLeft'}
-        heading={'Explore Scotish Nature'}
-        loremTop={1}
-        loremBtm={2}
-        img={scotNature}
+const ExploreWorldContent = () => (
+  <main className="contentGrid">
+    <div className={classes.heading}>
+      <h1 className={classes.headingH1}>Explore Scotland</h1>
+      <LoremIpsum p={1} />
+    </div>
+
+    <ContentPara
+      position="left"
+      imgPosition="imgLeft"
+      heading="Explore Scotish Nature"
+      loremTop={1}
+      loremBtm={2}
+      img={scotNature}
+    />
+    <div className={classes.paraSubR}>
+      <h4 className={classes.paraHeading}>Explore Scotish Cities</h4>
+      <LoremIpsum p={1} />
+      <img
+        className={`${classes.subImgR} ${classes.subImg}`}
+        src={scotCity}
+        alt="sub"
       />
-      <div className={classes.paraSubR}>
-        <h4 className={classes.paraHeading}>Explore Scotish Cities</h4>
-        <LoremIpsum p={1} />
-        <img
-          className={`${classes.subImgR} ${classes.subImg}`}
-          src={scotCity}
-          alt={'sub'}
-        ></img>
-        <LoremIpsum p={2} />
-      </div>
-      <div className={classes.paraSubL}>
-        <h4 className={classes.paraHeading}>Explore Scotish Nightlife</h4>
-        <LoremIpsum p={1} />
-        <img
-          className={`${classes.subImgL} ${classes.subImg}`}
-          src={scotNightlife}
-          alt={'sub'}
-        ></img>
-        <LoremIpsum p={1} />
-      </div>
-      <img className={classes.flag} src={scotFlag} alt={'flag'}></img>
-      {/* MUST SEE SECTION */}
-      <h3 className={classes.mustSeeHeading}>{null}</h3>
-      <div className={`${classes.mustSeeContent} ${classes.mustSeeTop}`}>
+      <LoremIpsum p={2} />
+    </div>
+    <div className={classes.paraSubL}>
+      <h4 className={classes.paraHeading}>Explore Scotish Nightlife</h4>
+      <LoremIpsum p={1} />
+      <img
+        className={`${classes.subImgL} ${classes.subImg}`}
+        src={scotNightlife}
+        alt="sub"
+      />
+      <LoremIpsum p={1} />
+    </div>
+    <img className={classes.flag} src={scotFlag} alt="flag" />
+    {/* MUST SEE SECTION */}
+    <h3 className={classes.mustSeeHeading}>{null}</h3>
+    <div className={`${classes.mustSeeContent} ${classes.mustSeeTop}`}>
+      <h5>Visit Placeholder</h5>
+      <img className={classes.mustSeeImg} src={mustSee} alt="must see" />
+    </div>
+    <div className={`${classes.mustSeeContent} ${classes.mustSeeMid}`}>
+      <h5>Visit Placeholder</h5>
+      <img className={classes.mustSeeImg} src={mustSee} alt="must see" />
+    </div>
+    <div className={`${classes.mustSeeContent} ${classes.mustSeeBtm}`}>
+      <h5>Visit Placeholder</h5>
+      <img className={classes.mustSeeImg} src={mustSee} alt="must see" />
+    </div>
+    <div className={classes.mustSeeCardContainer}>
+      <div className={`${classes.mustSeeContent} `}>
         <h5>Visit Placeholder</h5>
-        <img
-          className={classes.mustSeeImg}
-          src={mustSee}
-          alt={'must see'}
-        ></img>
+        <img className={classes.mustSeeImg} src={mustSee} alt="must see" />
       </div>
-      <div className={`${classes.mustSeeContent} ${classes.mustSeeMid}`}>
+      <div className={`${classes.mustSeeContent}`}>
         <h5>Visit Placeholder</h5>
-        <img
-          className={classes.mustSeeImg}
-          src={mustSee}
-          alt={'must see'}
-        ></img>
+        <img className={classes.mustSeeImg} src={mustSee} alt="must see" />
       </div>
-      <div className={`${classes.mustSeeContent} ${classes.mustSeeBtm}`}>
+      <div className={`${classes.mustSeeContent} `}>
         <h5>Visit Placeholder</h5>
-        <img
-          className={classes.mustSeeImg}
-          src={mustSee}
-          alt={'must see'}
-        ></img>
+        <img className={classes.mustSeeImg} src={mustSee} alt="must see" />
       </div>
-      <div className={classes.mustSeeCardContainer}>
-        <div className={`${classes.mustSeeContent} `}>
-          <h5>Visit Placeholder</h5>
-          <img
-            className={classes.mustSeeImg}
-            src={mustSee}
-            alt={'must see'}
-          ></img>
-        </div>
-        <div className={`${classes.mustSeeContent}`}>
-          <h5>Visit Placeholder</h5>
-          <img
-            className={classes.mustSeeImg}
-            src={mustSee}
-            alt={'must see'}
-          ></img>
-        </div>
-        <div className={`${classes.mustSeeContent} `}>
-          <h5>Visit Placeholder</h5>
-          <img
-            className={classes.mustSeeImg}
-            src={mustSee}
-            alt={'must see'}
-          ></img>
-        </div>
-      </div>
-    </main>
-  );
-};
+    </div>
+  </main>
+);
 
 export default ExploreWorldContent;
