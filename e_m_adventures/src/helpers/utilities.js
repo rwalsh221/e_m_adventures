7b8eff8b749dbc63ad72c4278323082efe187b00
Richard Walsh <richwalsh2@googlemail.com>
2021-11-30 12:00:00 +0000

    fix sunrise sunset formating on weather card

--- e_m_adventures/src/helpers/utilities.js
+++ e_m_adventures/src/helpers/utilities.js
@@ -5,12 +5,10 @@ export const formatTime = (unixTimeStamp) => {
   // Hours part from the timestamp
   const hours = date.getHours();
   // Minutes part from the timestamp
-  const minutes = `0 ${date.getMinutes()}`;
-  // Seconds part from the timestamp
-  const seconds = `0 ${date.getSeconds()}`;
+  const minutes = `0${date.getMinutes()}`;
 
   // Will display time in 10:30:23 format
-  return `${hours} : ${minutes.substr(-2)} : ${seconds.substr(-2)}`;
+  return `${hours} : ${minutes.substr(-2)}`;
 };
 
 export const formatDate = (unixTimeStamp) => {
