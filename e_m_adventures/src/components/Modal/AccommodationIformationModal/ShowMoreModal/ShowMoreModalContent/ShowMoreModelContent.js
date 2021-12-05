93e2439d23f5d02071e07fb602b67325b4eea9ec
Richard Walsh <richwalsh2@googlemail.com>
2021-12-05 15:11:33 +0000

    fix key error in ShowMoreModelContent

--- e_m_adventures/src/components/Modal/AccommodationIformationModal/ShowMoreModal/ShowMoreModalContent/ShowMoreModelContent.js
+++ e_m_adventures/src/components/Modal/AccommodationIformationModal/ShowMoreModal/ShowMoreModalContent/ShowMoreModelContent.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import classes from './showMoreModal.module.css';
+import { nanoid } from 'nanoid';
+import classes from './ShowMoreModal.module.css';
 
 const ShowMoreModal = ({ primaryContent, secondaryContent = null }) => {
   const primaryContentUl = primaryContent.additional.map((el) => (
-    <li>
+    <li key={nanoid()}>
       <div className={classes.ionIconContainer}>
         <ion-icon name="checkmark-circle" />
       </div>
       <p>{el}</p>
     </li>
   ));
+
   let secondaryContentHeading;
   let secondaryContentUl;
 
   if (secondaryContent) {
     secondaryContentHeading = <h3>You must acknowledge</h3>;
     secondaryContentUl = secondaryContent.additional.map((el) => (
-      <li>
+      <li key={nanoid()}>
         <div className={classes.ionIconContainer}>
           <ion-icon name="checkmark-circle" />
         </div>
