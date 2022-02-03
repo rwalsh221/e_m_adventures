27a4fb7a4d9a736a75ce09849c9a6f7fad864a77
Richard Walsh <richwalsh2@googlemail.com>
2022-02-03 11:00:20 +0000

    update accommodationCard css

--- e_m_adventures/src/components/Accommodation/AccommodationContent/AccommodationCard/AccommodationCard.js
+++ e_m_adventures/src/components/Accommodation/AccommodationContent/AccommodationCard/AccommodationCard.js
@@ -14,14 +14,18 @@ const AccommodationCard = ({
     onClick={() => onClickProps(idProps)}
     aria-hidden
   >
-    <h2 className={classes.accommodationCard__heading}>{nameProps}</h2>
+    <h2 className={classes.accommodationCard__heading} data-capitalizefont>
+      {nameProps}
+    </h2>
     <img
       className={classes.accommodationCard__img}
       src={`img/accommodation/${idProps}/hero.jpg`}
       alt={nameProps}
     />
     <p className={classes.accommodationCard__description}>{descriptionProps}</p>
-    <p className={classes.accommodationCard__location}>{cityProps}</p>
+    <p className={classes.accommodationCard__location} data-capitalizefont>
+      {cityProps}
+    </p>
     <p className={classes.accommodationCard__price}>
       {`£${priceProps} / night`}
     </p>
