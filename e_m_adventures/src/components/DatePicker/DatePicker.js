456925ad671d5ab106daea99cb080e5b357a2f6c
Richard Walsh <richwalsh2@googlemail.com>
2022-02-15 20:35:37 +0000

    continue with date picker

--- e_m_adventures/src/components/DatePicker/DatePicker.js
+++ e_m_adventures/src/components/DatePicker/DatePicker.js
@@ -1,8 +1,11 @@
+import { current } from '@reduxjs/toolkit';
 import { element } from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './DatePicker.module.css';
 
 const DatePicker = () => {
+  const [month, setMonth] = useState({ left: 1, right: 2 });
+
   const todayDate = new Date();
 
   const focusMonth = [];
@@ -27,35 +30,72 @@ const DatePicker = () => {
     return numDays;
   };
 
-  const getDays = (month) => {
+  // create seperate func for init setup. setstate for current mont and current month +1. then use getdays() for forward / back
+
+  const getDays = (month = null, arr) => {
     const currentDay = todayDate.getDate();
     const numDays = getNumDays(month);
     const d = new Date();
 
-    d.setMonth(month);
-    console.log(d.getTimezoneOffset());
+    if (month) {
+      d.setMonth(month);
+    }
+
+    // month ? d.setMonth(month) : d.setMonth(d.getMonth());
+
+    const x = new Date('July 21, 1983 01:15:00');
+
     // d.setMinutes(0);
     // d.setHours(0);
     for (let i = 1; i <= numDays; i += 1) {
-      i = i + 36000;
-      console.log(d.setDate(i));
-      focusMonth.push(d.setDate(i));
+      // i = i + 32;
+
+      d.setDate(i);
+
+      arr.push(d.toDateString());
     }
   };
 
-  console.log(getNumDays(todayDate.getMonth()));
-  getDays(6);
-  console.log(focusMonth);
+  const nextMonthHandler = () => {
+    const currentSetMonthLeft = month.left;
+    const currentSetMonthRight = month.right;
 
-  const content = focusMonth.map((element) => (
+    setMonth({
+      left: currentSetMonthLeft + 1,
+      right: currentSetMonthRight + 1,
+    });
+  };
+
+  const prevMonthHandler = () => {
+    const currentSetMonthLeft = month.left;
+    const currentSetMonthRight = month.right;
+
+    setMonth({
+      left: currentSetMonthLeft - 1,
+      right: currentSetMonthRight - 1,
+    });
+  };
+
+  getDays(month.left, focusMonth);
+  getDays(month.right, nextMonth);
+
+  const leftContent = focusMonth.map((element) => (
     <div className={classes.dayCard}>
       <p>{element}</p>
     </div>
   ));
 
+  const rightContent = nextMonth.map((element) => (
+    <div className={classes.dayCard}>
+      <p>{element}</p>
+    </div>
+  ));
   return (
-    <section>
-      <div className={classes.cardContainer}>{content}</div>
+    <section className={classes.datePicker}>
+      <div className={classes.leftCardContainer}>{leftContent}</div>
+      <div className={classes.rightCardContainer}>{rightContent}</div>
+      <button onClick={nextMonthHandler}>forward</button>
+      <button onClick={prevMonthHandler}>back</button>
     </section>
   );
 };
