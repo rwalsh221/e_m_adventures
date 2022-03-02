86236cb5525d5712bf0b202f98e7ab749bc458ac
Richard Walsh <richwalsh2@googlemail.com>
2022-03-02 19:19:26 +0000

    continue new search bar, start drop down menus for guest and location

---
+++ e_m_adventures/src/components/NewSearch/HeaderSearchGuestDD/HeaderSearchGuestDD.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './HeaderSearchGuestDD.moduele.css';
+
+const HeaderSearchGuestDD = () => {
+  return <div></div>;
+};
+
+export default HeaderSearchGuestDD;
