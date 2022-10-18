7edfac11048e71cfdf0e68165b357419def55e55
Richard Walsh <richwalsh2@googlemail.com>
2022-10-18 19:48:09 +0100

    fix  eslint errors

--- e_m_adventures/src/helpers/booking/bookingIsAvaliable.js
+++ e_m_adventures/src/helpers/booking/bookingIsAvaliable.js
@@ -2,7 +2,7 @@ import checkFullDays from './checkFullDays';
 
 const bookingIsAvaliable = (allBookings, currentBooking, setErrorState) => {
   if (allBookings === {}) return true;
-  console.log(allBookings);
+
   setErrorState('');
   // COMPARISON ARRAYS
   const checkInArr = [];
@@ -10,13 +10,13 @@ const bookingIsAvaliable = (allBookings, currentBooking, setErrorState) => {
   const fullDayArr = [];
 
   const allBookingsKeys = Object.keys(allBookings);
-  console.log(allBookingsKeys);
+
   // ADD CHECKIN, CHECKOUT, FULLDAY TO COMPARISON ARRAYS
   allBookingsKeys.forEach((el) => {
     // FULL DAYS
     if (allBookings[el].fullDays !== undefined) {
-      allBookings[el].fullDays.forEach((el) => {
-        fullDayArr.push(el);
+      allBookings[el].fullDays.forEach((element) => {
+        fullDayArr.push(element);
       });
     }
 
@@ -25,11 +25,10 @@ const bookingIsAvaliable = (allBookings, currentBooking, setErrorState) => {
     // CHECKOUT
     checkOutArr.push(allBookings[el].checkOut);
   });
-  console.log(currentBooking);
-  console.log(checkInArr);
+
   // SEARCH COMPARISON ARRAYS TO SEE IF NEW BOOKING IS AVALIABLE
   const passFullDay = checkFullDays(currentBooking, fullDayArr);
-  console.log(passFullDay);
+
   if (
     checkInArr.indexOf(currentBooking.checkIn) === -1 &&
     checkOutArr.indexOf(currentBooking.checkOut) === -1 &&
